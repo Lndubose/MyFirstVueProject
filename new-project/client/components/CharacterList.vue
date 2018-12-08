b65787fd4dbc868ed88bfda0e43e939c9658a272
lndubose <lndubose13@gmail.com>
2018-12-08 00:03:32 -0600

    Character List Component

---
+++ new-project/client/components/CharacterList.vue
@@ -0,0 +1,54 @@
+<template>
+  <div v-if="!!character.length" class="ui characters-list cards">
+    <div v-for="character in characters" :key="character.name" class="ui card fadeIn-animation">
+      <div>
+        <img :src="character.image">
+      </div>
+      <div class="content">
+        <div class="header">{{character.name}}</div>
+        <div class="description">{{character.description}}</div>
+      </div>
+      <div class="extra content">
+        <span class="right floated">
+          <a :href="character.url">
+            <button class="ui icon purple tiny button">More info</button>
+          </a>
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: mapGetters(["characters"])
+};
+</script>
+
+<style>
+.characters-list.cards {
+  margin-top: 2em;
+  justify-content: center;
+}
+.characters-list.cards .image {
+  position: relative;
+  width: 100%;
+  height: 246px;
+  overflow: hidden;
+}
+.characters-list.cards .image img {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  height: 100%;
+  width: auto;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+}
+.ui.card,
+.ui.cards > .card {
+  width: 364px;
+}
+</style>
+</style>
