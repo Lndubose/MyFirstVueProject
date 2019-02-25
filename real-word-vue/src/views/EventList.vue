a456d80fca11cff44092675eb01c4c2665996400
Lauren D <lndubose13@gmail.com>
2019-02-24 23:32:41 -0600

    Finish Real World Vue.js from Mastery Vue

---
+++ real-word-vue/src/views/EventList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Events Listing</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+import EventCard from "@/views/EventCard.vue";
+import EventService from "@/Services/EventService.js";
+export default {
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: []
+    };
+  },
+  created() {
+    EventService.getEvents()
+      .then(response => (this.events = response.data))
+      .catch(err => console.loge("There was an error:" + err.response));
+  }
+};
+</script>
